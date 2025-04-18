c98cc99b94249dcfac531a92970c57eb5770205d
mokafish <mokafish@users.noreply.github.com>
2025-04-18 09:44:47 +0800

    feat: update bin entry to point to the correct cli.js location in dist folder

---
+++ dist/cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import meow from 'meow';
+import supportsColor from 'supports-color';
+
+const cli = meow(`
+	Usage
+	  $ supports-color
+
+	Options
+	  --256  Check for 256 color support
+	  --16m  Check for 16 million color support
+
+	Examples
+	  $ supports-color
+	  $ supports-color --256
+	  $ supports-color --16m
+
+	Exits with code 0 if color is supported and 1 if not
+`);
+
+const {flags} = cli;
+const level = flags['256'] ? 'has256' : (flags['16m'] ? 'has16m' : 'hasBasic');
+
+console.log('hello world');
+console.log(level);
+console.log('\n// dist/cli.js');
+
+
