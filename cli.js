fdd5ab3c375c256574cec12d3dfa5a209bba6d35
mokafish <mokafish@users.noreply.github.com>
2025-04-18 10:44:22 +0800

    feat: 更新 CLI 示例，统一使用 bakahttp 命令

--- cli.js
+++ cli.js
@@ -2,22 +2,24 @@
 import meow from 'meow';
 const cli = meow(`
 	Usage
-	  $ supports-color
+	  $ bakahttp
 
 	Options
 	  --256  Check for 256 color support
 	  --16m  Check for 16 million color support
 
 	Examples
-	  $ supports-color
-	  $ supports-color --256
-	  $ supports-color --16m
+	  $ bakahttp
+	  $ bakahttp --256
+	  $ bakahttp --16m
 
 	Exits with code 0 if color is supported and 1 if not
 `);
 
 const {flags} = cli;
+const level = flags['256'] ? 'has256' : (flags['16m'] ? 'has16m' : 'hasBasic');
+
 console.log('hello world');
-console.log(flags);
+console.log(level);
 
 
