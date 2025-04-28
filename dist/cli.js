a088e7edaf95d518debe59ec40ad0bfb31bf3320
mokafish <mokafish@users.noreply.github.com>
2025-04-28 13:42:57 +0800

    feat: 更新 CLI 选项，添加延迟、请求单位、请求头、Cookies、请求体、HTTP 方法、代理、静默模式等功能

--- dist/cli.js
+++ dist/cli.js
@@ -16,11 +16,31 @@ const cli = meow(`
     $ ${pkg.name} [options] [args]
 
   Options
-    --concurrent, -c <num> 设置最大并发数
-    --help            显示帮助信息
+    -c, --concurrent <num>  Set concurrent workers (default: 16)
+    -d, --delay <min[-max]> Delay between cycle in milliseconds (default: 1-3)
+    -u, --unit <min[-max]>  Requests per cycle (default: 1)
+    -H, --header <k:v>      Add custom request header (repeatable)
+    -C, --cookies <file>    Load cookies.txt or cookies.json  from file
+    -b, --body <file>       File to use as request body
+    -m, --method <name>     HTTP method to use (default: GET; POST if has body)
+    -p, --proxy <url>       Proxy server to use (http/socks5)
+    -r, --run <task>        Task to run (default: tease)
+    -s, --silent            Suppress output logging
+    -o, --out <dir>         [?]Output directory for results
+        --out-stats <file>  File to save statistics
+        --out-log <file>    [?]File to save log
+        --http2             Use HTTP/2 protocol
+    -h, --help              Show this help
+    -v, --version           Show version
+
+  Arguments
+    1      Target URL with placeholder markers
+    ...    Replacement rules for URL placeholders
 
   Examples
-    $ ${pkg.name} mytask -c 10
+    $ ${pkg.name} -c 16 \\
+      https://example.com/?id={}&user={}&t={} \\
+      1- 1: ts
 `, {
   importMeta: import.meta,
   flags: {
@@ -29,24 +49,81 @@ const cli = meow(`
       shortFlag: 'c',
       default: 16
     },
-    task: {
+    delay: {
       type: 'string',
-      shortFlag: 't',
+      shortFlag: 'd',
+      default: '1-3'
+    },
+    unit: {
+      type: 'string',
+      shortFlag: 'u',
+      default: '1'
+    },
+    header: {
+      type: 'string',
+      shortFlag: 'H',
+      isMultiple: true
+    },
+    cookies: {
+      type: 'string',
+      shortFlag: 'C'
+    },
+    body: {
+      type: 'string',
+      shortFlag: 'b'
+    },
+    method: {
+      type: 'string',
+      shortFlag: 'm',
+      default: 'GET'
+    },
+    proxy: {
+      type: 'string',
+      shortFlag: 'p'
+    },
+    run: {
+      type: 'string',
+      shortFlag: 'r',
       default: 'tease'
+    },
+    silent: {
+      type: 'boolean',
+      shortFlag: 's'
+    },
+    out: {
+      type: 'string',
+      shortFlag: 'o'
+    },
+    outStats: {
+      type: 'string'
+    },
+    outLog: {
+      type: 'string'
+    },
+    http2: {
+      type: 'boolean'
+    },
+    help: {
+      type: 'boolean',
+      shortFlag: 'h'
+    },
+    version: {
+      type: 'boolean',
+      shortFlag: 'v'
     }
   }
 });
-let taskName = cli.flags.task;
-// let taskModule = await loadTask(taskName);
-// let taskClass =  taskModule.default 
-// let taskClass =  SleepTask;
+if (cli.flags.body && cli.flags.method == 'GET') {
+  cli.flags.method = 'POST';
+}
+
 /**
  * @type {typeof BaseTask}
  */
-let taskClass = (await import(`./tasks/${cli.flags.task}.js`)).default;
+let taskClass = (await import(`./tasks/${cli.flags.run}.js`)).default;
 await taskClass.parseArgs(cli.input, cli.flags);
 let bk = new Baka(taskClass, {});
-render(/*#__PURE__*/React.createElement(App, {
+cli.flags.silent || render(/*#__PURE__*/React.createElement(App, {
   baka: bk
 }));
 await bk.init();
