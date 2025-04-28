2eb362a7bc71024c783ba9f59fac64d5b10f8097
mokafish <mokafish@users.noreply.github.com>
2025-04-28 13:28:16 +0800

    feat: 更新 cli.js，移除对 SleepTask 和 TeaseTask 的引用，添加对 BaseTask 的支持

--- src/cli.js
+++ src/cli.js
@@ -4,8 +4,9 @@ import meow from 'meow';
 import React from 'react';
 import { render } from 'ink';
 import App from './view.js';
-import SleepTask from './tasks/sleep.js';
-import TeaseTask from './tasks/tease.js';
+// import SleepTask from './tasks/sleep.js';
+// import TeaseTask from './tasks/tease.js';
+import BaseTask from './tasks/base.js';
 // import pkg from '../package.json' with  { type: 'json' };
 // import { loadTask } from './services.js';
 import Baka from './baka.js';
@@ -44,7 +45,10 @@ let taskName = cli.flags.task;
 // let taskModule = await loadTask(taskName);
 // let taskClass =  taskModule.default 
 // let taskClass =  SleepTask;
-let taskClass =  TeaseTask;
+/**
+ * @type {typeof BaseTask}
+ */
+let taskClass =  (await import(`./tasks/${cli.flags.task}.js`)).default;
 await taskClass.parseArgs(cli.input, cli.flags);
 let bk = new Baka(taskClass, {})
 render(<App baka={bk} />);
